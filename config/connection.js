b6496012b961961c8f2fe12b46b036dbf3f99af3
cffhr99 <littlema@BAIYANGs-Mini.verizon.net>
2021-08-26 18:07:05 -0400

    add JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -2,9 +2,12 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
+
+const JAWSDB_URL='mysql://orqao9wv121ufy3b:ogp4tq9x3btkyfhj@s29oj5odr85rij2o.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ss66neixamdq4o9q'
+
 // create connection to our db
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
+const sequelize = JAWSDB_URL
+  ? new Sequelize(JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
