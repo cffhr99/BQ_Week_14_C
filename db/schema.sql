594b120a1e4add566e033dcaac0ccc7d166a082c
cffhr99 <littlema@BAIYANGs-Mini.verizon.net>
2021-08-26 17:43:39 -0400

    update database

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,3 @@
--- DROP DATABASE IF EXISTS tech_blog_db;
+DROP DATABASE IF EXISTS tech_blog_db;
 
--- CREATE DATABASE tech_blog_db;
-
-DROP DATABASE IF EXISTS ss66neixamdq4o9q;
-
-CREATE DATABASE ss66neixamdq4o9q;
+CREATE DATABASE tech_blog_db;
