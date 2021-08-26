ab8b15a16331a24107106df435e21e22a4f534e9
cffhr99 <littlema@BAIYANGs-Mini.verizon.net>
2021-08-26 17:40:18 -0400

    connect to jawsdb

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,7 @@
-DROP DATABASE IF EXISTS tech_blog_db;
+-- DROP DATABASE IF EXISTS tech_blog_db;
 
-CREATE DATABASE tech_blog_db;
+-- CREATE DATABASE tech_blog_db;
+
+DROP DATABASE IF EXISTS ss66neixamdq4o9q;
+
+CREATE DATABASE ss66neixamdq4o9q;
